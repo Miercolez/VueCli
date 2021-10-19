3dde1946dab9c86be86c04d70642ff230e2c1c30
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-10-19 09:42:58 +0200

    lagt upp

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyBooks/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyBooks from "@/components/MyBooks";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyBooks
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
